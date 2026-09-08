f600a2daea567a08a257ad3efd26ad417013b5e7
Akshata03-keyu <navalurshreya27@gmail.com>
2026-09-08 10:29:53 +0530

    Add files via upload

---
+++ Java if else.java
@@ -0,0 +1,19 @@
+class Main {
+    public static void main(String[] args) {
+        int age = 20;
+
+        if (age <= 13)
+            System.out.println("Child");
+        else ifc (age <= 18)
+            System.out.println("Teen");
+        else if (age <= 40)
+            System.out.println("Major");
+        else if (age <= 60)
+            System.out.println("Matured");
+        else if (age <= 80)
+            System.out.println("Senior");
+        else 
+            System.out.println("baby");x
+
+    }
+}
---
+++ Positive and negative.java
@@ -0,0 +1,12 @@
+ class Main {
+    public static void main(String[] args) {
+        int num = -2;
+
+        if (num > 0)
+            System.out.println( "positive number");
+        else
+            System.out.println( "negative number");
+    }
+}
+        
+            
